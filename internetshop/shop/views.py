756e09d1c9be5f38994960cd60024ce732713772
sashatt14 <aleksashatt@gmail.com>
2024-08-26 13:42:00 +0500

    Удалила ноут,сделала цикл с продукт

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    products=Product.objects.all()
+
+    return render(request,"index.html",{
+        "products": products
+    })
                 
 
